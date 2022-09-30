8a8d501bb40020727a3082f88f5028565b90fe04
juliarosabfs <92101856+juliarosabfs@users.noreply.github.com>
2022-09-30 09:41:29 -0300

    Update kwitter_page.js

--- kwitter_page.js
+++ kwitter_page.js
@@ -34,10 +34,9 @@ function getData() { firebase.database().ref("/"+room_name).on('value', function
 	       name = message_data['name'];
 	       message = message_data['message'];
          like = message_data['like'];
-         name_with_tag = "<h4> "+ name +"<img class='user_tick' src='tick.png'></h4>";
+         name_with_tag = "<h4> "+ name +"</h4>";
          message_with_tag = "<h4 class='message_h4'>" + message + "</h4>";
-like_button ="<button class='btn btn-warning' id="+firebase_message_id+" value="+like+" onclick='updateLike(this.id)'>";
-         span_with_tag = "<span class='glyphicon glyphicon-thumbs-up'>Curtidas: "+ like +"</span></button><hr>";
+like_button ="<button class='btn btn-warning' id="+firebase_message_id+" value="+like+" onclick='updateLike(this.id)'>Curtidas: "+ like +"</button><hr>";
 
         row = name_with_tag + message_with_tag +like_button + span_with_tag;       
         document.getElementById("output").innerHTML += row;
